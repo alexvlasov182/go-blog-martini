adaae1ece362dbff78ba5ce75982f7dad94ba240
Александр Власов <drumlife182@gmail.com>
2022-01-02 23:40:20 +0200

    add package

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"blog-martini/blog-martini/models"
 	"net/http"
 
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
-	"go-blog-martini/models"
 )
 
 var posts map[string]*models.Post
@@ -61,12 +61,12 @@ func main() {
 	m := martini.Classic()
 
 	m.Use(render.Renderer(render.Options{
-		Directory: "templates", // Specify what path to load the templates from.
-		Layout: "layout", // Specify a layout template. Layouts can call {{ yield }} to render the current template.
+		Directory:  "templates",                // Specify what path to load the templates from.
+		Layout:     "layout",                   // Specify a layout template. Layouts can call {{ yield }} to render the current template.
 		Extensions: []string{".tmpl", ".html"}, // Specify extensions to load for templates.
 		//Funcs: []template.FuncMap{AppHelpers}, // Specify helper function maps for templates to access.
-		Charset: "UTF-8", // Sets encoding for json and html content-types. Default is "UTF-8".
-		IndentJSON: true, // Output human readable JSON
+		Charset:    "UTF-8", // Sets encoding for json and html content-types. Default is "UTF-8".
+		IndentJSON: true,    // Output human readable JSON
 		//IndentXML: true, // Output human readable XML
 		//HTMLContentType: "application/xhtml+xml", // Output XHTML content type instead of default "text/html"
 	}))
